ea68b302223574112e001747ce168ce69d7c5728
fikymar <marcela.jaklova@gmail.com>
2023-05-01 20:42:36 +0200

    fout issue

--- src/root.tsx
+++ src/root.tsx
@@ -33,7 +33,7 @@ export default component$(() => {
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 					<link
-						href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@300;400;500&display=swap"
+						href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@300;400;500&display=block"
 						rel="stylesheet"
 					/>
 				</head>
